ba626e85a0c5fc065727bf518be02dd189e3b460
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-09-06 20:03:36 -0300

    Created a file JWTextField to implement my class later

---
+++ Demo/JWTextField.h
@@ -0,0 +1,13 @@
+//
+//  JWTextField.h
+//  Demo
+//
+//  Created by Jhonathan Wyterlin on 06/09/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JWTextField : UITextField
+
+@end
---
+++ Demo/JWTextField.m
@@ -0,0 +1,21 @@
+//
+//  JWTextField.m
+//  Demo
+//
+//  Created by Jhonathan Wyterlin on 06/09/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "JWTextField.h"
+
+@implementation JWTextField
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
